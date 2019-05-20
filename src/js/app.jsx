1e61c8da79ca1794366e40d450a54608b6a1caf6
Alan Vieceli <alansvieceli@gmail.com>
2019-05-20 20:06:26 -0300

    Cartão Basico

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -3,12 +3,18 @@ import React from 'react'
 
 import Titulo from './titulo/titulo'
 import Navbar from './navbar/navbar'
+import Cartao from './cartao/cartao'
 
 let App = (
     <div>
         <Navbar  titulo="React" cor="red"/>
         <div className="container">
             <Titulo />
+            <div className="row">
+                <div className="col m4"> 
+                    <Cartao />
+                </div>
+            </div>
         </div>
     </div>
 );
