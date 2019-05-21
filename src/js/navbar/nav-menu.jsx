daa5a2bd17f7b78c0681581d031c6900240ce3d2
Alan Vieceli <alansvieceli@gmail.com>
2019-05-20 22:54:29 -0300

    manipulando estado

--- src/js/navbar/nav-menu.jsx
+++ src/js/navbar/nav-menu.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 
 class NavMenu extends React.Component{
-    render(){
 
-        let lista = this.props.menu.map(function(value, index){
-            return (
-              <li key={value.titulo}><a href={value.link}>{value.titulo}</a></li>
-            );
-          });
+  constructor(props){
+    super(props);
+    this.state = {menuAtivo: "Home"}
+    this.alterarActive = this.alterarActive.bind(this);
+  }
+
+  alterarActive(self, titulo){
+    self.setState({menuAtivo: titulo});
+  }
+
+  render(){
+
+    let self = this;
+
+    let lista = this.props.menu.map(function(value, index){
+          return (
+              <li onClick={self.alterarActive.bind(null, self, value.titulo)} key={value.titulo} className={self.state.menuAtivo == value.titulo ? 'active' : '' }><a href={value.link}>{value.titulo}</a></li>
+          );
+        });
 
         return (
           <ul id="nav-mobile" className="right">
--- src/js/navbar/navbar.jsx
+++ src/js/navbar/navbar.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import NavMenu from './nav-menu'
 
 class Navbar extends React.Component{
+
+    constructor(props){
+      super(props);
+      this.state = {menuAtivo: "Home"}
+
+    }
+
     render(){
         let menu = [
           {titulo: "Home", link: "#home"},
