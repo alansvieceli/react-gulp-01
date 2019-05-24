e3e55734925a41c0b9c18f1d4a5b49ce05fea41a
Alan Vieceli <alansvieceli@gmail.com>
2019-05-24 19:38:07 -0300

    Busca

--- src/js/cartao/lista-cartao.jsx
+++ src/js/cartao/lista-cartao.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Cartao from './cartao'
+import Busca from '../busca/busca'
 
 class ListaCartao extends React.Component{
     constructor(props){
         super(props);
-        this.state = {cliques: 0}
+        this.state = {cliques: 0, busca: '', dados: [], servidor: []};
         this.addClique = this.addClique.bind(this);
+        this.atualizaBusca = this.atualizaBusca.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
     }
 
     addClique(){
@@ -15,17 +18,67 @@ class ListaCartao extends React.Component{
         }));
     }
 
-    render(){
-        let noticias = [
-            {titulo: "Titulo 1", descricao: "Descrição", detalhe: "Detalhe comm um texto legal", imagem: "https://materializecss.com/images/office.jpg", link: "#teste"},
-            {titulo: "Titulo 2", descricao: "Descrição", detalhe: "Detalhe comm um texto legal", imagem: "https://materializecss.com/images/office.jpg", link: "#teste"},
-            {titulo: "Titulo 3", descricao: "Descrição", detalhe: "Detalhe comm um texto legal", imagem: "https://materializecss.com/images/office.jpg", link: "#teste"},
-            {titulo: "Titulo 4", descricao: "Descrição", detalhe: "Detalhe comm um texto legal", imagem: "https://materializecss.com/images/office.jpg", link: "#teste"},
-            {titulo: "Titulo 5", descricao: "Descrição", detalhe: "Detalhe comm um texto legal", imagem: "https://materializecss.com/images/office.jpg", link: "#teste"},
-            {titulo: "Titulo 6", descricao: "Descrição", detalhe: "Detalhe comm um texto legal", imagem: "https://materializecss.com/images/office.jpg", link: "#teste"},
-            {titulo: "Titulo 7", descricao: "Descrição", detalhe: "Detalhe comm um texto legal", imagem: "https://materializecss.com/images/office.jpg", link: "#teste"}
-        ];
+    atualizaBusca(evento){        
+        this.setState({busca: evento.target.value});
+        if(evento.target.value == ""){
+            this.setState({dados: this.state.servidor});
+        }
+    }
 
+    onSubmit(evento){        
+        let busca = this.state.busca;
+        let dados = this.state.servidor;
+
+        let novaLista = dados.filter(function(item){
+          if (item.titulo.toUpperCase().indexOf(busca.toUpperCase()) > -1
+          || item.descricao.toUpperCase().indexOf(busca.toUpperCase()) > -1
+          || item.detalhe.toUpperCase().indexOf(busca.toUpperCase()) > -1) {
+            return item;
+          }
+        });
+
+        this.setState({dados: novaLista});        
+    
+        evento.preventDefault();
+      }
+    
+      componentDidMount(){
+
+        this.setState({
+            dados: [
+                {titulo: "Titulo 1", descricao: "Descrição", detalhe: "Detalhe comm um texto legal", imagem: "https://materializecss.com/images/office.jpg", link: "#teste"},
+                {titulo: "Titulo 2", descricao: "Descrição", detalhe: "Detalhe comm um texto legal", imagem: "https://materializecss.com/images/office.jpg", link: "#teste"},
+                {titulo: "Titulo 3", descricao: "Descrição", detalhe: "Detalhe comm um texto legal", imagem: "https://materializecss.com/images/office.jpg", link: "#teste"},
+                {titulo: "Titulo 4", descricao: "Descrição", detalhe: "Detalhe comm um texto legal", imagem: "https://materializecss.com/images/office.jpg", link: "#teste"},
+                {titulo: "Titulo 5", descricao: "Descrição", detalhe: "Detalhe comm um texto legal", imagem: "https://materializecss.com/images/office.jpg", link: "#teste"},
+                {titulo: "Titulo 6", descricao: "Descrição", detalhe: "Detalhe comm um texto legal", imagem: "https://materializecss.com/images/office.jpg", link: "#teste"},
+                {titulo: "Titulo 7", descricao: "Descrição", detalhe: "Detalhe comm um texto legal", imagem: "https://materializecss.com/images/office.jpg", link: "#teste"}
+            ],
+            servidor: [
+                {titulo: "Titulo 1", descricao: "Descrição", detalhe: "Detalhe comm um texto legal", imagem: "https://materializecss.com/images/office.jpg", link: "#teste"},
+                {titulo: "Titulo 2", descricao: "Descrição", detalhe: "Detalhe comm um texto legal", imagem: "https://materializecss.com/images/office.jpg", link: "#teste"},
+                {titulo: "Titulo 3", descricao: "Descrição", detalhe: "Detalhe comm um texto legal", imagem: "https://materializecss.com/images/office.jpg", link: "#teste"},
+                {titulo: "Titulo 4", descricao: "Descrição", detalhe: "Detalhe comm um texto legal", imagem: "https://materializecss.com/images/office.jpg", link: "#teste"},
+                {titulo: "Titulo 5", descricao: "Descrição", detalhe: "Detalhe comm um texto legal", imagem: "https://materializecss.com/images/office.jpg", link: "#teste"},
+                {titulo: "Titulo 6", descricao: "Descrição", detalhe: "Detalhe comm um texto legal", imagem: "https://materializecss.com/images/office.jpg", link: "#teste"},
+                {titulo: "Titulo 7", descricao: "Descrição", detalhe: "Detalhe comm um texto legal", imagem: "https://materializecss.com/images/office.jpg", link: "#teste"}
+            ]
+        });
+
+        /*
+        let self = this;        
+        axios.get('http://localhost:8000/servidor.php?dados=1').then(function(response){
+          self.setState({
+            dados: response.data,
+            servidor: response.data
+          });
+        });
+        */
+    
+      }
+
+    render(){
+        let noticias = this.state.dados;
         let aux = [];
         let novaLista = [];
         for (let i=0; i < noticias.length; i++){
@@ -64,6 +117,9 @@ class ListaCartao extends React.Component{
 
         return (
           <div>
+              <div className="row">
+                  <Busca atualizaBusca={this.atualizaBusca} onSubmit={this.onSubmit} busca={this.state.busca}/>
+              </div>
               <p>Quantidade de cliques: {this.state.cliques}</p>
               {linha}
           </div>
